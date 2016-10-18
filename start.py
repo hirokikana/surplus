e7942c22c768119aefbde51fcd02ebf0aab746c4
Hiroki Takayasu <hiroki.kana@gmail.com>
2016-10-19 01:45:57 +0900

    [add] item_id burden rate api [add] payment_type / payment_id

--- start.py
+++ start.py
@@ -17,10 +17,6 @@ class CashBookAccess():
                                  all())
         return json.dumps(result)
 
-    def hoge(self, x):
-        x.update({'expense':x['expense'] * rate})
-        return x
-    
     def find_by_user_id(self, user_id):
         result = []
         for burden_rate_row in session.query(BurdenRateString).filter(BurdenRateString.user_id == user_id).all():
@@ -62,7 +58,9 @@ class CashBookAccess():
                 'use_date':row.use_date.isoformat(),
                 'item':row.item,
                 'expense':row.expense,
-                'income':row.income
+                'income':row.income,
+                'payment_type':row.payment_type,
+                'payment_id':row.payment_id
             })
         return result
 
@@ -73,13 +71,17 @@ def index():
 
 @route('/api/v1/cashbook', method='POST')
 def post_cashbook():
-    # nkf -m0Z1 ~/hoge.csv|grep '^[0-9]'|awk -F',' '{print $4","$5","$6}'
+    # nkf -m0Z1 ~/hoge.csv|grep '^[0-9]'|awk -F',' '{print "hoge,"$1","$4","$5","$6}'
+    # CSV format : payment_type, payment_id, use_date, item, expense
+    # test : curl -X POST -F csv=@hoge.csv http://localhost:8080/api/v1/cashbook  
     reader = csv.reader(request.files.get('csv').file)
     for row in reader:
-        use_date = datetime.datetime.strptime(row[0], '%Y/%m/%d').date()
-        item = row[1].decode('utf-8')
-        expense = row[2]
-        session.add(CashBook(item=item, expense=expense, use_date=use_date))
+        payment_type = row[0]
+        payment_id = row[1]
+        use_date = datetime.datetime.strptime(row[2], '%Y/%m/%d').date()
+        item = row[3].decode('utf-8')
+        expense = row[4]
+        session.add(CashBook(payment_type=payment_type, payment_id=payment_id, item=item, expense=expense, use_date=use_date))
     session.commit()
 
 @route('/api/v1/cashbook')
@@ -114,17 +116,31 @@ def cashbook_find_by_user_year_month(user_id, year, month):
     end_date = '%d-%02d' % (year, month+1)
     return template('{{!json}}', json=CashBookAccess().find_by_user_id_and_date_range(user_id, start_date, end_date))
 
-@route('/api/v1/burdenrate/user/<user_id:int>', method='POST')
-def set_burden_rate(user_id):
-    item_string = request.forms.get('item_string')
+@route('/api/v1/burdenrate/item_string', method='POST')
+def set_burden_rate_item_string():
+    user_id = request.forms.get('user_id')
+    item_string = request.forms.get('item_string').decode('utf-8')
     rate = request.forms.get('rate')
     session.add(BurdenRateString(item_string=item_string, rate=rate, user_id=user_id))
+    session.commit()
+    return template('{"result":"ok"}')
+
+@route('/api/v1/burdenrate/item_id', method='POST')
+def set_burnden_rate_item_id():
+    user_id = request.forms.get('user_id')
+    item_id = request.forms.get('item_id')
+    rate = request.forms.get('rate')
+    session.add(BurdenRateItemId(item_id=item_id, rate=rate, user_id=user_id))
+    session.commit()
     return template('{"result":"ok"}')
     
 @route('/api/v1/burdenrate/user/<user_id:int>')
 def get_burden_rate(user_id):
-    result = session.query(BurdenRateString).filter(BurdenRateString.user_id == user_id).all()
-    return template('{{!json}}', json=json.dumps([{'id':x.id,'item_string':x.item_string,'rate':x.rate} for x in result]))
+    result_string = session.query(BurdenRateString).filter(BurdenRateString.user_id == user_id).all()
+    result_item_id = session.query(BurdenRateItemId).filter(BurdenRateItemId.user_id == user_id).all()
+    session.commit()
+    result = [{'id':x.id,'item_string':x.item_string,'rate':x.rate} for x in result_string] + [{'id':x.id,'item_id':x.item_id,'rate':x.rate} for x in result_item_id]
+    return template('{{!json}}', json=json.dumps(result))
 
 
 Base = declarative_base()
@@ -132,6 +148,8 @@ Base = declarative_base()
 class CashBook(Base):
     __tablename__ = 'cashbook'
     id = Column(Integer, primary_key=True)
+    payment_type = Column(String)
+    payment_id = Column(Integer)
     use_date = Column(Date)
     item = Column(String)
     income = Column(Integer, default=0)
